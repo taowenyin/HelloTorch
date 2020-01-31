c6e8df028d60e5611b56b8ae29dbd811d6384553
taowenyin <wenyin.tao@163.com>
2020-01-31 13:55:02 +0800

    添加卷积

--- S4.py
+++ S4.py
@@ -11,6 +11,8 @@ import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
 
+from common.ConvNet import ConvNet
+
 # 图像的尺寸
 image_size = 28
 # 标签的种类数
@@ -19,6 +21,12 @@ num_class = 10
 num_epochs = 10
 # 批次大小
 batch_size = 64
+
+def rightness(predictions, labels):
+    """计算预测错误率的函数，其中predictions是模型给出的一组预测结果，batch_size行num_classes列的矩阵，labels是数据之中的正确答案"""
+    pred = torch.max(predictions.data, 1)[1] # 对于任意一行（一个样本）的输出值的第1个维度，求最大，得到每一行的最大元素的下标
+    rights = pred.eq(labels.data.view_as(pred)).sum() #将下标与labels中包含的类别进行比较，并累计得到比较正确的数量
+    return rights, len(labels) #返回正确的数量和这一次一共比较了多少元素
 
 
 if __name__ == '__main__':
@@ -42,9 +50,35 @@ if __name__ == '__main__':
     test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size,
                              shuffle=False,  sampler=sample_test)
 
-    idx = 100
-    muteimg = train_dataset[idx][0].numpy()
-    plt.imshow(muteimg[0, ...])
-    plt.show()
-    print('标签是：', train_dataset[idx][1])
+    # 构建网络对象
+    net = ConvNet(image_size, num_class)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+    # 记录准确率等数值
+    record = []
+    # 记录卷积核
+    weights = []
+
+    for epoch in range(num_epochs):
+        # 记录训练数据集准确率的容器
+        train_rights = []
+
+        for batch_idx, (data, target) in enumerate(train_loader):
+            net.train()
+            output = net(data)
+            loss = criterion(output, target)
+            optimizer.zero_grad()
+            optimizer.step()
+            right = rightness(output, target)
+            train_rights.append(right)
+
+            if batch_idx % 100 == 0:
+                net.eval()
+                val_rights = []
+
+                # 验证集
+                for(data, target) in validation_loader:
+                    output = net(data)
+                    right = rightness(output, target)
+                    val_rights.append(right)
--- common/ConvNet.py
+++ common/ConvNet.py
@@ -1,17 +1,24 @@
+from typing import Any
+
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
 # 定义两个卷积层的厚度
 depth = [4, 8]
 
 
 class ConvNet(nn.Module):
+    # 图像大小
+    __image_size = 0
+    # 分类类别
+    __num_classes = 0
+
     def __init__(self, image_size, num_classes):
         super(ConvNet, self).__init__()
-        # 图像大小
-        self.image_size = image_size
-        # 分类类别
-        self.num_classes = num_classes
+        self.__image_size = image_size
+        self.__num_classes = num_classes
 
         # 定义第一层的卷积层
         self.conv1 = nn.Conv2d(1, 4, 5, padding=2)
@@ -20,4 +27,33 @@ class ConvNet(nn.Module):
         # 定义第二层的卷积层
         self.conv2 = nn.Conv2d(depth[0], depth[1], 5, padding=2)
         # 定义一个线性连接
-        self.fc1 = nn.Linear(image_size // 4 * image_size // 4 * depth[1], 512)
+        self.fc1 = nn.Linear(self.__image_size // 4 * self.__image_size // 4 * depth[1], 512)
+        # 定义最后一个线性分类单元，输出为类别
+        self.fc2 = nn.Linear(512, self.__num_classes)
+
+    def forward(self, x):
+        # 第一层卷积(batch_size, image_channels, image_width, image_height)
+        x = self.conv1(x)
+        # 使用ReLU激活函数，防止过拟合(batch_size, num_filters, image_width, image_height)
+        x = F.relu(x)
+        # 第二层池化(batch_size, depth[0], image_width/2, image_height/2)
+        x = self.pool(x)
+        # 第三层卷积(batch_size, depth[1], image_width/2, image_height/2)
+        x = self.conv2(x)
+        x = F.relu(x)
+        # 第四层池化(batch_size, depth[1], image_width/4, image_height/4)
+        x = self.pool(x)
+        # 把数据变为一维数据(batch_size, depth[1] * image_width/4 * image_height/4)
+        x = x.view(-1, self.__image_size // 4 * self.__image_size // 4 * depth[1])
+        # 第五层为全链接层(batch_size, 512)
+        x = F.relu(self.fc1(x))
+        # 随即丢弃，防止过拟合
+        x = F.dropout(x, training=self.training)
+        # 第六层全链接层(batch_size, num_classes)
+        x = self.fc2(x)
+        x = F.log_softmax(x, dim=1)
+
+        return x
+
+
+
