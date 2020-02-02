ce154e761bfb7362499f0cb395f32c86ebe61ea5
taowenyin <wenyin.tao@163.com>
2020-02-02 19:02:46 +0800

    完成卷积网络

--- S4.py
+++ S4.py
@@ -22,11 +22,15 @@ num_epochs = 10
 # 批次大小
 batch_size = 64
 
+
+# 计算预测的损失函数
 def rightness(predictions, labels):
-    """计算预测错误率的函数，其中predictions是模型给出的一组预测结果，batch_size行num_classes列的矩阵，labels是数据之中的正确答案"""
-    pred = torch.max(predictions.data, 1)[1] # 对于任意一行（一个样本）的输出值的第1个维度，求最大，得到每一行的最大元素的下标
-    rights = pred.eq(labels.data.view_as(pred)).sum() #将下标与labels中包含的类别进行比较，并累计得到比较正确的数量
-    return rights, len(labels) #返回正确的数量和这一次一共比较了多少元素
+    # 计算每批中每个元素概率最大的索引
+    pred = torch.max(predictions.data, 1)[1]
+    # 将预测的下标标签与实际标签进行比较，并求和，得到正确的数量
+    rights = pred.eq(labels.data.view_as(pred)).sum()
+    # 返回正确的数量和这一次一共比较了多少元素
+    return rights, len(labels)
 
 
 if __name__ == '__main__':
@@ -50,6 +54,16 @@ if __name__ == '__main__':
     test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size,
                              shuffle=False,  sampler=sample_test)
 
+    # 数据读取测试
+    # idx = 100
+    # img = train_dataset[idx]
+    # img_data = img[0][0].numpy()
+    # img_label = img[1]
+    #
+    # plt.imshow(img_data)
+    # plt.show()
+    # print('标签是：', img_label)
+
     # 构建网络对象
     net = ConvNet(image_size, num_class)
     criterion = nn.CrossEntropyLoss()
@@ -65,20 +79,79 @@ if __name__ == '__main__':
         train_rights = []
 
         for batch_idx, (data, target) in enumerate(train_loader):
-            net.train()
-            output = net(data)
-            loss = criterion(output, target)
+            data, target = data.clone(), target.clone().detach()
+
+            # 清空梯度
             optimizer.zero_grad()
+            # 打开dropout标记
+            net.train()
+            # 完成一次前馈计算，并得到预测值
+            output = net(data)
+            # 计算损失函数
+            loss = criterion(output, target)
+            # 反向传播
+            loss.backward()
+            # 梯度下降法
             optimizer.step()
+
             right = rightness(output, target)
+            # 记录预测损失函数结果
             train_rights.append(right)
 
+            # 每执行100个批次打印一次结果
             if batch_idx % 100 == 0:
+                # 关闭dropout标记
                 net.eval()
+                # 记录在校验集上的准确度
                 val_rights = []
 
                 # 验证集
                 for(data, target) in validation_loader:
+                    data, target = data.clone(), target.clone().detach()
                     output = net(data)
                     right = rightness(output, target)
                     val_rights.append(right)
+
+                # 获取在训练集上的正确的数量和样本数量
+                train_r = (sum([tup[0] for tup in train_rights]), sum([tup[1] for tup in train_rights]))
+                # 获取在验证集上的正确的数量和样本数量
+                val_r = (sum([tup[0] for tup in val_rights]), sum([tup[1] for tup in val_rights]))
+
+                # 打印验证集和训练集情况
+                print(val_r)
+                print('训练周期: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\t训练正确率: {:.2f}%\t校验正确率: {:.2f}%'.format(
+                    epoch,
+                    batch_idx * batch_size, len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader),
+                    loss.data,
+                    100. * train_r[0].numpy() / train_r[1],
+                    100. * val_r[0].numpy() / val_r[1]))
+
+                # 记录训练集和验证集的错误率
+                record.append((100 - 100. * train_r[0] / train_r[1], 100 - 100. * val_r[0] / val_r[1]))
+
+                # 记录训练过程中权重
+                weights.append([net.conv1.weight.data.clone(), net.conv1.bias.data.clone(),
+                                net.conv2.weight.data.clone(), net.conv2.bias.data.clone()])
+
+    plt.plot(record)
+    plt.xlabel('Steps')
+    plt.ylabel('Error rate')
+    plt.show()
+
+    # 关闭dropout标记
+    net.eval()
+    # 记录准确率
+    vals = []
+
+    # 在测试集上进行测试
+    for data, target in test_loader:
+        data, target = data.clone(), target.clone().detach()
+        output = net(data)
+        val = rightness(output, target)
+        vals.append(val)
+
+    # 计算精确度
+    rights = (sum([tup[0] for tup in vals]), sum([tup[1] for tup in vals]))
+    right_rate = 100. * rights[0].numpy() / rights[1]
+    print('测试集的精度：', right_rate)
--- common/ConvNet.py
+++ common/ConvNet.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 depth = [4, 8]
 
 
+# 卷积神经
 class ConvNet(nn.Module):
     # 图像大小
     __image_size = 0
@@ -31,29 +32,57 @@ class ConvNet(nn.Module):
         # 定义最后一个线性分类单元，输出为类别
         self.fc2 = nn.Linear(512, self.__num_classes)
 
-    def forward(self, x):
-        # 第一层卷积(batch_size, image_channels, image_width, image_height)
-        x = self.conv1(x)
-        # 使用ReLU激活函数，防止过拟合(batch_size, num_filters, image_width, image_height)
-        x = F.relu(x)
-        # 第二层池化(batch_size, depth[0], image_width/2, image_height/2)
-        x = self.pool(x)
-        # 第三层卷积(batch_size, depth[1], image_width/2, image_height/2)
-        x = self.conv2(x)
-        x = F.relu(x)
-        # 第四层池化(batch_size, depth[1], image_width/4, image_height/4)
-        x = self.pool(x)
-        # 把数据变为一维数据(batch_size, depth[1] * image_width/4 * image_height/4)
-        x = x.view(-1, self.__image_size // 4 * self.__image_size // 4 * depth[1])
-        # 第五层为全链接层(batch_size, 512)
-        x = F.relu(self.fc1(x))
-        # 随即丢弃，防止过拟合
-        x = F.dropout(x, training=self.training)
-        # 第六层全链接层(batch_size, num_classes)
-        x = self.fc2(x)
-        x = F.log_softmax(x, dim=1)
+    # image_size = image_size + 2 * padding - kernel_size + step
+    # 实现前向运算
+    def forward(self, *input: Any, **kwargs: Any):
+        # 获取输入的数据(batch_size, image_channels, image_width, image_height)
+        data = input[0]
 
-        return x
+        # 第一层卷积(batch_size, image_channels, image_size, image_size)
+        data = self.conv1(data)
+        # 使用ReLU激活函数，防止过拟合
+        data = F.relu(data)
+        # 第一层池化(batch_size, image_channels, image_size / pool_size, image_size / pool_size)
+        data = self.pool(data)
 
+        # 第二层卷积
+        data = self.conv2(data)
+        # 使用ReLU激活函数，防止过拟合
+        data = F.relu(data)
+        # 第二层池化
+        data = self.pool(data)
 
+        # 将立方体的特征图转化为一维向量
+        data = data.view(-1, data.shape[1] * data.shape[2] * data.shape[3])
+
+        # 第三层全链接层
+        data = self.fc1(data)
+        # 使用ReLU激活函数，防止过拟合
+        data = F.relu(data)
+
+        # 执行dropout，防止过拟合
+        data = F.dropout(data, training=self.training)
+
+        # 第四层全链接层，输出结果
+        data = self.fc2(data)
+        # 使用softmax激活函数
+        data = F.log_softmax(data, dim=0)
+
+        return data
+
+    # 保存特征图
+    def record_features(self, data):
+        # 第一层卷积
+        data = self.conv1(data)
+        # 使用ReLU激活函数，防止过拟合
+        feature_map1 = F.relu(data)
+        # 第一层池化
+        data = self.pool(feature_map1)
+
+        # 第二层卷积
+        data = self.conv2(data)
+        # 使用ReLU激活函数，防止过拟合
+        feature_map2 = F.relu(data)
+
+        return feature_map1, feature_map2
 
