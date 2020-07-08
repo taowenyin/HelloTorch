d5a581b793b77d919757c797ed5effc0a3396228
taowenyin <wenyin.tao@163.com>
2020-07-08 23:03:37 +0800

    解释数据预处理

--- utils/abide/prepare_data.py
+++ utils/abide/prepare_data.py
@@ -30,33 +30,45 @@ from sklearn.model_selection import StratifiedKFold, train_test_split
 from utils.abide.prepare_utils import (load_phenotypes, format_config, run_progress, hdf5_handler)
 
 
+# 计算每个ROI的连接性
 def compute_connectivity(functional):
     with np.errstate(invalid="ignore"):
+        # 计算每个ROI的相关性
         corr = np.nan_to_num(np.corrcoef(functional))
+        # 相关性矩阵的一半可以去掉
         mask = np.invert(np.tri(corr.shape[0], k=-1, dtype=bool))
+        # 获得蒙版
         m = ma.masked_where(mask == 1, mask)
+        # 根据蒙版获取数据
         return ma.masked_where(m, corr).compressed()
 
 
 def load_patient(subj, tmpl):
+    # 拼接参数获取数据地址，并读取数据
     df = pd.read_csv(format_config(tmpl, {
         "subject": subj,
     }), sep="\t", header=0)
     df = df.apply(lambda x: pd.to_numeric(x, errors='coerce'))
 
+    # 获取ROI区域编号
     ROIs = ["#" + str(y) for y in sorted([int(x[1:]) for x in df.keys().tolist()])]
-
+    # 使用0替代无效元素，一共200行，表示200个感兴趣区域，每行一共有196个元素，表示每个感兴趣区域有196个值
     functional = np.nan_to_num(df[ROIs].to_numpy().T).tolist()
+    # axis=1表示沿着x轴数据标准化
     functional = preprocessing.scale(functional, axis=1)
+    # 计算并获得每两个ROI之间的连接性
     functional = compute_connectivity(functional)
     functional = functional.astype(np.float32)
-
+    # 返回某个病人的ROI的连接性
     return subj, functional.tolist()
 
 
+# 启动线程载入患者数据
 def load_patients(subjs, tmpl, jobs=1):
+    # 构建函数对象
     partial_load_patient = partial(load_patient, tmpl=tmpl)
     msg = "Processing {current} of {total}"
+    # 启动线程载入患者数据，并返回字典数据
     return dict(run_progress(partial_load_patient, subjs, message=msg, jobs=jobs))
 
 
@@ -90,6 +102,7 @@ def prepare_folds(hdf5, folds, pheno, derivatives, experiment):
             # fold["test"] = ids[test_index].tolist()
 
 
+# 把患者的数据保存到HDF5中
 def load_patients_to_file(hdf5, pheno, derivatives):
 
     download_root = "../../data/ABIDE/functionals"
@@ -101,27 +114,29 @@ def load_patients_to_file(hdf5, pheno, derivatives):
         "ho": "cpac/filt_global/rois_ho/{subject}_rois_ho.1D",
         "tt": "cpac/filt_global/rois_tt/{subject}_rois_tt.1D",
     }
-    #print('storing_patients')
+    # 创建患者分组，并返回对象
     storage = hdf5.require_group("patients")
+    # 获取患者的文件ID
     file_ids = pheno["FILE_ID"].tolist()
-    #print('storing_finished')
 
+    # 遍历所有的脑图谱
     for derivative in derivatives:
-
-        #print('derivative_loop')
+        # 拼接脑图谱文件
         file_template = os.path.join(download_root, derivatives_path[derivative])
-        # print('one_over')
+        # 把患者数据文件转化为每个病人的每个ROI连接性数据
         func_data = load_patients(file_ids, tmpl=file_template)
-        #print('two_over')
 
         for pid in func_data:
             print('func_data_filling')
             record = pheno[pheno["FILE_ID"] == pid].iloc[0]
+            # 每个病人创建一个分组
             patient_storage = storage.require_group(pid)
+            # 保存每个病人的个人数据
             patient_storage.attrs["id"] = record["FILE_ID"]
             patient_storage.attrs["y"] = record["DX_GROUP"]
             patient_storage.attrs["site"] = record["SITE_ID"]
             patient_storage.attrs["sex"] = record["SEX"]
+            # 保存每个病人的数据
             patient_storage.create_dataset(derivative, data=func_data[pid])
 
 if __name__ == "__main__":
@@ -144,12 +159,11 @@ if __name__ == "__main__":
     valid_derivatives = ["cc200", "aal", "ez", "ho", "tt", "dosenbach160"]
     derivatives = [derivative for derivative in arguments["<derivative>"] if derivative in valid_derivatives]
 
-    #  如果没有患者数据，那么就要读取患者的MRI图片
+    #  如果没有患者数据，那么就要读取患者的ROI的连接性数据并把数据保存到HDF5中
     if "patients" not in hdf5:
         load_patients_to_file(hdf5, pheno, derivatives)
 
     if arguments["--whole"]:
-        
         print ("Preparing whole dataset")
         prepare_folds(hdf5, folds, pheno, derivatives, experiment="{derivative}_whole")
 
