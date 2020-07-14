29e3b5f833babecf8fc362c477c1e8aa9945a74f
taowenyin <wenyin.tao@163.com>
2020-07-14 23:38:27 +0800

    添加自编码器模型

--- AbideAutoEncoder.py
+++ AbideAutoEncoder.py
@@ -76,6 +76,8 @@ if __name__ == '__main__':
     code_size_2 = 600
     # 第一个自编码器的去噪率
     denoising_rate = 0.7
+    # 每批数据的大小
+    batch_size = 100
 
     # 定义训练、验证、测试数据
     X_train = y_train = X_valid = y_valid = X_test = y_test = 0
@@ -110,41 +112,32 @@ if __name__ == '__main__':
                 "experiment": experiment_cv,
             })
 
+    # 创建训练数据集
+    # train_dataset = TensorDataset(
+    #     torch.from_numpy(X_train).float().clone().detach().requires_grad_(True),
+    #     torch.from_numpy(np.array(y_train).reshape(-1, 1)).float().clone().detach().requires_grad_(True))
+    train_dataset = TensorDataset(
+        torch.from_numpy(X_train).float().clone().detach(),
+        torch.from_numpy(np.array(y_train).reshape(-1, 1)).float().clone().detach())
+    # 创建测试数据集
+    test_dataset = TensorDataset(
+        torch.from_numpy(X_test).float().clone().detach(),
+        torch.from_numpy(np.array(y_test).reshape(-1, 1)).float().clone().detach())
+    # 创建验证数据集
+    validation_dataset = TensorDataset(
+        torch.from_numpy(X_valid).float().clone().detach(),
+        torch.from_numpy(np.array(y_valid).reshape(-1, 1)).float().clone().detach())
 
-    # 构建噪声
-    noise = np.random.uniform(0, 1 - denoising_rate, X_train.shape)
+    # 创建训练数据加载器，并且设置每批数据的大小，以及每次读取数据时随机打乱数据
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    # 创建验证集加载器
+    validation_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+    # 测试集加载器
+    test_loader = DataLoader(dataset=validation_dataset, batch_size=batch_size, shuffle=True)
 
     a = AutoEncoderModel(1000, [50, 20, 30], 700)
     print('xxx')
 
-    # # 每批数据的大小
-    # batch_size = 100
-    #
-    # # 创建数据
-    # dataset = MnistData()
-    # train_labels, train_data = dataset.parse_train_data()
-    # test_labels, test_data = dataset.parse_test_target()
-    # validation_labels, validation_data = dataset.parse_validation_data()
-    #
-    # # 创建训练数据集
-    # train_dataset = TensorDataset(
-    #     train_data.float().clone().detach().requires_grad_(True),
-    #     train_labels.float().clone().detach().requires_grad_(True))
-    # # 创建训练数据集
-    # test_dataset = TensorDataset(
-    #     test_data.float().clone().detach().requires_grad_(True),
-    #     test_labels.float().clone().detach().requires_grad_(True))
-    # # 创建验证数据集
-    # validation_dataset = TensorDataset(
-    #     validation_data.float().clone().detach().requires_grad_(True),
-    #     validation_labels.float().clone().detach().requires_grad_(True))
-    #
-    # # 创建训练数据加载器，并且设置每批数据的大小，以及每次读取数据时随机打乱数据
-    # train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-    # # 创建验证集加载器
-    # validation_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
-    # # 测试集加载器
-    # test_loader = DataLoader(dataset=validation_dataset, batch_size=batch_size, shuffle=True)
     #
     # # 创建模型
     # model = AutoEncoderModel(28, 28, AutoEncoderType.Multi)
