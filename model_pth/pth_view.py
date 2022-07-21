68bb9ff61938a5ccb1f238378d764870c3567bf1
taowenyin <wenyin.tao@163.com>
2022-07-22 00:49:17 +0800

    添加pth的可视化

---
+++ model_pth/pth_view.py
@@ -0,0 +1,23 @@
+import torch
+
+
+if __name__ == '__main__':
+    content_1 = torch.load('cmt_tiny.pth')
+    content_2 = torch.load('faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth')
+
+    # 修改键值
+    old_val = content_1['model']
+    content_1['state_dict'] = content_1.pop('model')
+    torch.save(content_1, './cmt_tiny_mm.pth')
+
+    changed_dict = torch.load('./cmt_tiny_mm.pth')
+    print(old_val.keys())
+    print(changed_dict['state_dict'].keys())
+
+    # 查看键值
+    # print(content_1.keys())
+    # print(content_2.keys())
+    #
+    # print(content_1['state_dict'].keys())
+    # print(content_2['state_dict'].keys())
+    # print(content['state_dict'].keys())
