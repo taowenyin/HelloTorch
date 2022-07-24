c3a8284979eb3e6a1d47abfe87400321074320f3
taowenyin <wenyin.tao@163.com>
2022-07-24 16:08:51 +0800

    完善PTH文件的操作

--- dataset/labelme/labelme2coco.py
+++ dataset/labelme/labelme2coco.py
@@ -10,7 +10,14 @@ np.random.seed(41)
 
 # 0为背景
 classname_to_id = {
-    "fire": 1
+    "A": 1,
+    "B": 2,
+    "C": 3,
+    "D": 4,
+    "E": 5,
+    "F": 6,
+    "G": 7,
+    "H": 8,
 }
 
 
--- model_pth/pth_view.py
+++ model_pth/pth_view.py
@@ -1,19 +1,71 @@
 import torch
 
 # https://www.cnblogs.com/codehome/p/16453848.html
+# https://www.cnblogs.com/sddai/p/14949982.html
+
+
+class PthUtil:
+    def __init__(self):
+        print('===初始化PTH工具===')
+
+    def view_keys(self, pth_model):
+        print(pth_model.keys())
+
+    def save_model(self, pth_model, save_path):
+        torch.save(pth_model, save_path)
+
+    def modify_key_name(self, pth_path, root_key, old_key, new_key):
+        pth_model = torch.load(pth_path, map_location=torch.device('cpu'))
+        pth_model[root_key][new_key] = pth_model[root_key].pop(old_key)
+
+        torch.save(pth_model, pth_path)
+
+    def modify_root_key_name(self, pth_path, old_key, new_key):
+        pth_model = torch.load(pth_path, map_location=torch.device('cpu'))
+        pth_model[new_key] = pth_model.pop(old_key)
+
+        torch.save(pth_model, pth_path)
+
+    def delete_keys(self, pth_path, root_key, keys):
+        pth_model = torch.load(pth_path, map_location=torch.device('cpu'))
+        # key.startswith('decoder1')
+        for key in list(pth_model[root_key].keys()):
+            if key in keys:
+                del pth_model[root_key][key]
+
+        torch.save(pth_model, pth_path)
+
+    def change_key_value(self, pth_path, root_key, key, value):
+        pth_model = torch.load(pth_path, map_location=torch.device('cpu'))
+        for _key in list(pth_model[root_key].keys()):
+            if _key == key:
+                pth_model[root_key][_key] = value
+                break
+
+        torch.save(pth_model, pth_path)
+
 
 if __name__ == '__main__':
-    content_1 = torch.load('cmt_tiny.pth')
-    content_2 = torch.load('faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth')
+    util = PthUtil()
+    pth_model = torch.load('./cmt_tiny_mm.pth', map_location=torch.device('cpu'))
+    # util.view_keys(pth_model['state_dict'])
+    # util.view_keys(pth_model)
 
-    # 修改键值
-    old_val = content_1['model']
-    content_1['state_dict'] = content_1.pop('model')
-    torch.save(content_1, './cmt_tiny_mm.pth')
+    util.delete_keys('./cmt_tiny_mm.pth', 'state_dict', ['head.weight', 'head.bias', '_fc.weight', '_fc.bias',
+                                                         '_bn.weight', '_bn.bias', '_bn.running_mean',
+                                                         '_bn.running_var', '_bn.num_batches_tracked',
+                                                         'relative_pos_a', 'relative_pos_b',
+                                                         'relative_pos_c', 'relative_pos_d'])
 
-    changed_dict = torch.load('./cmt_tiny_mm.pth')
-    print(old_val.keys())
-    print(changed_dict['state_dict'].keys())
+
+    # # 修改键值
+    # old_val = content_1['model']
+    # content_1['state_dict'] = content_1.pop('model')
+    # torch.save(content_1, './cmt_tiny_mm.pth')
+    #
+    # changed_dict = torch.load('./cmt_tiny_mm.pth')
+    # print(old_val.keys())
+    # print(changed_dict['state_dict'].keys())
 
     # 查看键值
     # print(content_1.keys())
